014a3424b4bf6dfe2ba4bcdbd66fc01a7b06750f
Ali Fahed <alifahed67@gmail.com>
2022-09-29 23:21:30 +0800

    Create portfolio components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import "./app.scss";
+import Navbar from "./components/navbar/Navbar";
+import Header from "./components/header/Header";
+import About from "./components/about/About";
+import Skills from "./components/skills/Skills";
+import Projects from "./components/projects/Projects";
+import Contact from "./components/contact/Contact";
+
 function App() {
-  return <div className="app">My portfolio</div>;
+  return (
+    <div className="app">
+      <Navbar />
+      <div className="sections">
+        <Header />
+        <About />
+        <Skills />
+        <Projects />
+        <Contact />
+      </div>
+    </div>
+  );
 }
 
 export default App;
