2d513a76e705519ef7a351d9e95c9adeaa42975d
Ali Fahed <alifahed67@gmail.com>
2022-10-03 22:57:17 +0800

    Add Header content

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,18 @@
 import "./header.scss";
+import { KeyboardArrowDown } from "@material-ui/icons";
 
 const Header = () => {
-  return <div className="header" id="header"></div>;
+  return (
+    <div className="header" id="header">
+      <div className="hero-section">
+        <h1 className="greet">Hi, I am Ali Fahed</h1>
+        <p className="interest">A Web Developer</p>
+      </div>
+      <a href="#about">
+        <KeyboardArrowDown className="arrow-down" />
+      </a>
+    </div>
+  );
 };
 
 export default Header;
