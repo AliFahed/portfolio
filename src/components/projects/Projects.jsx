92e01770149fa8cbc87c3597d831542c15c40599
Ali Fahed <alifahed67@gmail.com>
2022-10-09 19:42:12 +0800

    Add Projects Section Content

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,7 +1,69 @@
 import "./projects.scss";
+import { projectsData } from "../../projectsData";
 
 const Projects = () => {
-  return <div className="projects" id="projects"></div>;
+  return (
+    <div className="projects" id="projects">
+      <div className="wrapper">
+        <h2 className="projectsTitle">My Projects</h2>
+        {projectsData.map((project, index) => (
+          <div key={index}>
+            <div className="projectsContainer">
+              <div className="projectContainer">
+                <img
+                  src={project.img}
+                  alt="project ScreenShoot"
+                  className="projectImg"
+                />
+                <div className="projectDetailsContainer">
+                  <h2 className="projectTitle">{project.title}</h2>
+                  <div className="projectTechStack">
+                    {project.techStack.map((tech, index) => (
+                      <div key={index}>
+                        <div className="techStack">
+                          <img
+                            src={tech.icon}
+                            alt="Tech Icon"
+                            className="techStackIcon"
+                          />
+                          <div>{tech.name}</div>
+                        </div>
+                      </div>
+                    ))}
+                  </div>
+                  <div className="projectDescription">
+                    <p>{project.desc}</p>
+                  </div>
+                  <div className="projectButtons">
+                    <div className="projectCode">
+                      <a
+                        href={project.links.github}
+                        className="code"
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        Review Code
+                      </a>
+                    </div>
+                    <div className="projectLive">
+                      <a
+                        href={project.links.live}
+                        className="live"
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        See It Live
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Projects;
---
+++ src/projectsData.js
@@ -0,0 +1,150 @@
+export const projectsData = [
+  {
+    id: 1,
+    img: "assets/shopcom-project.png",
+    title: "ShopCom",
+    techStack: [
+      {
+        name: "React",
+        icon: "assets/react-icon.svg",
+      },
+      {
+        name: "CSS",
+        icon: "assets/css-icon.svg",
+      },
+    ],
+    desc: "ShopCom have the best nike's shoe collection. Users can select and add items, the selected items will automatically be viewed in the shopping cart, including the total price.",
+    links: {
+      github: "https://github.com/AliFahed/shopping-cart",
+      live: "https://alifahed.github.io/shopping-cart/",
+    },
+  },
+  {
+    id: 2,
+    img: "assets/mini-message-board-project.png",
+    title: "Mini Message Borad",
+    techStack: [
+      {
+        name: "Express.js",
+        icon: "assets/express-icon.svg",
+      },
+      {
+        name: "Pug",
+        icon: "assets/pug-icon.svg",
+      },
+      {
+        name: "CSS",
+        icon: "assets/css-icon.svg",
+      },
+    ],
+    desc: "A Mini Message Board App for chatting. Users can view messages & send a message by filling a small form that includes their name & the message to be sent.",
+    links: {
+      github: "https://github.com/AliFahed/mini-message-board",
+      live: "https://calm-springs-97378.herokuapp.com/",
+    },
+  },
+  {
+    id: 3,
+    img: "assets/my-portfolio-project.png",
+    title: "My Portfolio Website",
+    techStack: [
+      {
+        name: "React",
+        icon: "assets/react-icon.svg",
+      },
+      {
+        name: "SASS",
+        icon: "assets/sass-icon.svg",
+      },
+    ],
+    desc: "I designed & created my portfolio website. I built reusable component (templete that is used for each project) for the whole projects section, to easily add, update, or remove projects.",
+    links: {
+      github: "https://github.com/AliFahed/portfolio",
+      live: "",
+    },
+  },
+  {
+    id: 4,
+    img: "assets/odin-library-project.png",
+    title: "Library",
+    techStack: [
+      {
+        name: "JavaScript",
+        icon: "assets/javascript-icon.svg",
+      },
+      {
+        name: "CSS",
+        icon: "assets/css-icon.svg",
+      },
+    ],
+    desc: "Library application that help users to track their progress on the books that is inserted in the library, select whether the book is read or not, or remove specific book from the library.",
+    links: {
+      github: "https://github.com/AliFahed/Odin-Library",
+      live: "https://alifahed.github.io/Odin-Library/",
+    },
+  },
+  {
+    id: 5,
+    img: "assets/restaurant-page-project.png",
+    title: "Restaurant Page",
+    techStack: [
+      {
+        name: "JavaScript",
+        icon: "assets/javascript-icon.svg",
+      },
+      {
+        name: "Webpack",
+        icon: "assets/webpack-icon.svg",
+      },
+      {
+        name: "CSS",
+        icon: "assets/css-icon.svg",
+      },
+    ],
+    desc: "Pizza hot is an Italian Restaurant that servers the best pizza in italy. The restaurant have a website that includes the restaurant menu & their contact information.",
+    links: {
+      github: "https://github.com/AliFahed/restaurant-page",
+      live: "https://alifahed.github.io/restaurant-page/",
+    },
+  },
+  {
+    id: 6,
+    img: "assets/signup-form-project.png",
+    title: "Signup Form",
+    techStack: [
+      {
+        name: "HTML",
+        icon: "assets/html-icon.svg",
+      },
+      {
+        name: "CSS",
+        icon: "assets/css-icon.svg",
+      },
+    ],
+    desc: "A simple Sign-up page for an imaginary webiste called 'Nature', Users need to enter their details in the form to sign-up in the website. Made to practice forms & from validation.",
+    links: {
+      github: "https://github.com/AliFahed/Sign-up-Form",
+      live: "https://alifahed.github.io/Sign-up-Form/",
+    },
+  },
+  {
+    id: 7,
+    img: "assets/etch-a-sketch-project.png",
+    title: "Etch-A-Sketch",
+    techStack: [
+      {
+        name: "JavaScript",
+        icon: "assets/javascript-icon.svg",
+      },
+      {
+        name: "CSS",
+        icon: "assets/css-icon.svg",
+      },
+    ],
+    desc: "Etch-A-Sketch is a mechanical drawing toy consists of a Board and different choice of colors including Black, Grey, Random Colors. Users can etch a sketch with the colors, erase or reset the sketch. Also the size of squares in the sketch can be modified by the user.",
+    links: {
+      github: "https://github.com/AliFahed/Etch-A-Sketch",
+      live: "https://alifahed.github.io/Etch-A-Sketch/",
+    },
+  },
+];
